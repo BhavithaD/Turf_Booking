b12bef54fa51c43e84366aef0cfdd07d7b5dfd5d
BhavithaD <bhavithadommaraju@gmail.com>
2024-05-18 11:14:09 +0530

    Initial commit

---
+++ src/main/java/Turf_booking/Spring_Boot/TurfBookingApplication.java
@@ -0,0 +1,13 @@
+package Turf_booking.Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurfBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurfBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Turf_booking/Spring_Boot/TurfBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package Turf_booking.Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurfBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
